7ca000602a3d435c91b9b560bd18fb576431c76f
dragoocho <deagoocho@gmail.com>
2021-03-15 22:20:23 +0900

    textInputHandler() 구현

--- canvasMain.js
+++ canvasMain.js
@@ -1,23 +1,37 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
 const fileInputBtn = document.getElementById("fileInputBtn");
 fileInputBtn.addEventListener("change", imageInputUpload);
 
-const canvas = document.getElementById("jsCanvas");
-const ctx = canvas.getContext("2d");
+const textInputValue = document.getElementById("textInputValue");
+textInputValue.addEventListener("change", textInputHandler);
+
+canvas.addEventListener("mousedown", mouseDownHandler, false);
 
 function imageInputUpload(e) {
   let reader = new FileReader();
   let img = new Image();
-  if (img) {
-    reader.onload = function (event) {
-      img.onload = function () {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0);
-      };
-      img.src = event.target.result;
+
+  reader.onload = function (event) {
+    img.onload = function () {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
     };
-    reader.readAsDataURL(e.target.files[0]);
-  } else {
-    alert("이미지 파일이 아님니다.");
-  }
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+}
+
+function mouseDownHandler(e) {
+  ctx.fillStyle = "red";
+  ctx.font = "28px georgia";
+  ctx.textAlign = "center";
+  ctx.fillText(textInputValue.value, e.offsetX, e.offsetY);
+}
+
+function textInputHandler(e) {
+  e.preventDefault();
+  textInputValue.value = e.target.value;
 }
