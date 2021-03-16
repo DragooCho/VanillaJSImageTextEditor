1bdb7f996b748001762ba983887b10ad66903164
dragoocho <deagoocho@gmail.com>
2021-03-16 13:39:49 +0900

    폰트타입을 한번 이상 불러와야 다음에 바로 폰트타입이 바뀌는 버그존재

--- canvasMain.js
+++ canvasMain.js
@@ -42,8 +42,8 @@ const optionSelectFont = document.getElementById("optionSelectFont");
 optionSelectFont.addEventListener("change", fontTypeInputHandler);
 let fontStyleValue = optionSelectFont.value;
 
-function fontTypeInputHandler(e) {
-  fontStyleValue = e.target.value;
+async function fontTypeInputHandler(e) {
+  fontStyleValue = await e.target.value;
 }
 
 //////////////////////////////////
