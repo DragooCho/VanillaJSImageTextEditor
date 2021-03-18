83c949f640545ed8e78eb1d5069d843b2ee1631f
dragoocho <deagoocho@gmail.com>
2021-03-19 00:01:17 +0900

    css 조정

--- js_Feature_set/textSizeHandler.js
+++ js_Feature_set/textSizeHandler.js
@@ -1,7 +1,7 @@
 const textSizeSlider = document.getElementById("myRange");
 const Tachometer = document.getElementById("sizeValueTachometer");
 let textSizeValue = textSizeSlider.value;
-Tachometer.innerHTML = textSizeSlider.value;
+Tachometer.innerHTML = textSizeValue;
 
 textSizeSlider.oninput = function () {
   Tachometer.innerHTML = this.value;
