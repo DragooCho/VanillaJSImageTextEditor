332f24651e23af98794c901268631ff1e48f7413
dragoocho <deagoocho@gmail.com>
2021-03-21 01:16:22 +0900

    코드정리

--- js_Feature_set/canvasMain.js
+++ js_Feature_set/canvasMain.js
@@ -9,13 +9,4 @@ function mouseDownHandler(e) {
   ctx.textBaseline = "middle";
   ctx.font = `${textSizeValue}px ${fontStyleValue}`;
   ctx.fillText(textInputValue.value, e.offsetX, e.offsetY);
-  console.log(e);
-}
-
-canvas.addEventListener("mousemove", textMoveHandler, false);
-function textMoveHandler(e) {
-  const textMovePointer = document.getElementById("mouseTextMove");
-  textMovePointer.innerText = textInputValue.value;
-  textMovePointer.style.left = e.clientX + "px";
-  textMovePointer.style.top = e.clientY + "px";
 }
