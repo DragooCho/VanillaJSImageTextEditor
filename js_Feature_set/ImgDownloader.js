f956bda4159c4281981c1b548db19543d454a1b8
dragoocho <deagoocho@gmail.com>
2021-05-04 11:14:33 +0900

    ImgDownloader.js 파일과 관련 html수정

--- js_Feature_set/ImgDownloader.js
+++ js_Feature_set/ImgDownloader.js
@@ -1,15 +1,20 @@
-const createDownloadImg = () => {
+const createImg = document.querySelector(".clickCreateImg");
+createImg.addEventListener("click", createDownloadImg, false);
+
+function createDownloadImg() {
   let dataURL = bgCanvas.toDataURL("image/png");
   dataURL = dataURL.replace(
     /^data:image\/[^;]*/,
     "data:application/octet-stream"
   );
+
   dataURL = dataURL.replace(
     /^data:application\/octet-stream/,
     "data:application/octet-stream;headers=Content-Disposition%3A%20attachment%3B%20filename=Canvas.png"
   );
+
   const ATagImage = document.createElement("a");
   ATagImage.download = "canvasImage.png";
   ATagImage.href = dataURL;
   ATagImage.click();
-};
+}
