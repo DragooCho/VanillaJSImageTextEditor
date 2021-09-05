585967077f5e8ae83abbbdf742ca35c5d48803aa
dragoocho <deagoocho@gmail.com>
2021-09-05 18:23:28 +0900

    파일을 가져올 때 이미지 파일이 아니면 경고창 뜨는 함수 구현

--- js_Feature_set/ImageUploadZone.js
+++ js_Feature_set/ImageUploadZone.js
@@ -1,14 +1,21 @@
 const fileInputBtn = document.getElementById("fileInputBtn");
 fileInputBtn.addEventListener("change", imageInputUpload);
 
+const fileCheckingWarning = (file, flieSrc) => {
+  const fileFormat = flieSrc.src.split("/")[0].split(":")[1];
+  if (fileFormat !== `${file}`) {
+    alert(`${file} 파일형식으로 넣어주세요!!!`);
+  }
+};
+
 function imageInputUpload(e) {
   let reader = new FileReader();
   let img = new Image();
 
   reader.onload = function (event) {
     img.onload = function () {
-      imgX = img.width;
-      imgY = img.height;
+      const imgX = img.width;
+      const imgY = img.height;
 
       img.height = 600 * ((imgY * 100) / imgX / 100);
       img.width = 600;
@@ -22,6 +29,8 @@ function imageInputUpload(e) {
       bgCtx.drawImage(img, 0, 0, bgCanvas.width, bgCanvas.height);
     };
     img.src = event.target.result;
+
+    fileCheckingWarning("image", img);
   };
   reader.readAsDataURL(e.target.files[0]);
 }
