dfc34abe254b7bfdab81ca9ad8276596dcbabc34
dragoocho <deagoocho@gmail.com>
2021-09-27 23:42:20 +0900

    모달 경고창 오류해결

--- js_Feature_set/modal_window.js
+++ js_Feature_set/modal_window.js
@@ -4,22 +4,22 @@ const container = document.querySelector("container");
 
 const fileCheckingWarning = (flieSrc) => {
   const fileFormat = flieSrc.src.match(/data:image/);
-
-  if (!fileFormat) {
-    modal.style.display = "flex";
-    container.style.display = "none";
-    modalBg.classList.add("modal_BG");
-  }
-};
-
-const modalOut = () => {
-  modal.style.display = "none";
-  container.style.display = "block";
-  modalBg.classList.remove("modal_BG");
+  if (!fileFormat) setModal();
 };
 
 modal.addEventListener("click", modalOut);
-// modalBg.addEventListener("click", modalOut);
+
+function setModal() {
+  modal.style.display = "flex";
+  container.style.display = "none";
+  modalBg.classList.add("modal_BG");
+}
+
+function modalOut() {
+  modal.style.display = "none";
+  container.style.display = "block";
+  modalBg.classList.remove("modal_BG");
+}
 
 // const fileCheckingWarning = (file, flieSrc) => {
 //   const fileFormat = flieSrc.src.match(/data:image/);
