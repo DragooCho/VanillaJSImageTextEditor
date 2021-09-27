feb840ec343aad51bab7606b0d75a460e2cb180f
dragoocho <deagoocho@gmail.com>
2021-09-27 15:35:50 +0900

    모달 경고창 관련 js 코드수정

--- js_Feature_set/modal_window.js
+++ js_Feature_set/modal_window.js
@@ -1,10 +1,3 @@
-// const fileCheckingWarning = (file, flieSrc) => {
-//   const fileFormat = flieSrc.src.split("/")[0].split(":")[1];
-//   if (fileFormat !== `${file}`) {
-//     alert(`${file} 파일형식으로 넣어주세요!!!`);
-//   }
-// };
-
 const fileCheckingWarning = (file, flieSrc) => {
   const fileFormat = flieSrc.src.match(/data:image/);
 
