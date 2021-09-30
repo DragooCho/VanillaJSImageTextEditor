1eb1d3f891c0b3aa2e40cd28b62a71b762e0a5b1
dragoocho <deagoocho@gmail.com>
2021-09-30 11:27:52 +0900

    컬러피커 추가

--- js_Feature_set/TextColorSelecter.js
+++ js_Feature_set/TextColorSelecter.js
@@ -1,7 +1,15 @@
 const selectTextColor = document.getElementById("selectTextColor");
+const colorPicker = document.getElementById("color_picker");
+
 selectTextColor.addEventListener("change", textColorInputHandler);
+colorPicker.addEventListener("input", colorPickerInputHandler);
+
 let textColorValue = selectTextColor.value;
 
 function textColorInputHandler(e) {
+  textColorValue = e.target.value;
+  colorPicker.value = e.target.value;
+}
+function colorPickerInputHandler(e) {
   textColorValue = e.target.value;
 }
--- js_Feature_set/textSizeHandler.js
+++ js_Feature_set/textSizeHandler.js
@@ -4,6 +4,7 @@ let textSizeValue = textSizeSlider.value;
 Tachometer.innerHTML = textSizeValue;
 
 textSizeSlider.oninput = function printTextScreen() {
+  console.log(this);
   Tachometer.innerHTML = this.value;
   textSizeValue = this.value;
 };
